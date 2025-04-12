932e06c1b58059b9e0803ed4dc0993db862ba5e7
mfrickss <mfricks777@gmail.com>
2025-04-11 22:30:41 -0300

    sla

--- basics/math.php
+++ basics/math.php
@@ -31,6 +31,8 @@
         <input type="text" name="x"><br>
         <label for="">y:</label>
         <input type="text" name="y"><br>
+        <label for="">z:</label>
+        <input type="text" name="z"><br>
         <input type="submit" value="total">
     </form>
 </body>
@@ -40,14 +42,20 @@
 
     $x = $_POST["x"];
     $y = $_POST["y"];
+    $z = $_POST["z"];
     $total = null;
 
     //$total = abs($x);
     //$total = round($x);
     //$total = floor($x);
     //$total = ceil($x);
-    //$total = pow($x, $y);
-    $total = sqrt($x);
+    // $total = sqrt($x, $y, $z);
+    //$total = pow($x, $y); 
+    // $total = max($x, $y, $z);  
+    // $total = min($x, $y, $z);
+    // $total = pi();
+    // $total = rand(152,158);
+
     echo $total;
 
 ?>
